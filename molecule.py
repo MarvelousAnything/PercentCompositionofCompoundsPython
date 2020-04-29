7f655b3d17b8a11ba18de57a20cc02fb3ffc5bc6
Thearchermancoding <owen.v.hayes@gmail.com>
2020-04-28 23:25:52 -0700

    Updated Molecule Class

--- molecule.py
+++ molecule.py
@@ -1,6 +1,16 @@
+from calculate import table
+import calculate
 
+#TODO: add more to Molecule class 
 class Molecule:
-    def __init__(self, elements, amounts):
+    def __init__(self, name, elements, amounts):
+        self.name = name
         self.molecule = dict(zip(elements, amounts))
+        self.amounts = amounts
+        self.elements = elements
+        self.mass = sum([table[a]["atomic_mass"]*b for a,b in zip(elements, amounts)])
+        self.compositions = calculate.compositions_from_empirical(self.elements, self.amounts)[1]
+
     def __str__(self):
-        return "".join([str(a)+str(self.molecule[a]) for a in self.molecule])
+        return self.name + " - " + ("".join([str(a)+str(self.molecule[a]) for a in self.molecule]))
+    
